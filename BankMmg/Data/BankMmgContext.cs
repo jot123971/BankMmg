9cc2167dc2c6afda53d13a87699c6e72843441c9
Harjot Singh <HS061@atcltd.atc.org.nz>
2021-02-22 09:59:20 +1300

    Add project files.

---
+++ BankMmg/Data/BankMmgContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BankMmg.Models;
+
+namespace BankMmg.Data
+{
+    public class BankMmgContext : DbContext
+    {
+        public BankMmgContext (DbContextOptions<BankMmgContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BankMmg.Models.BankSystem> BankSystem { get; set; }
+    }
+}
---
+++ BankMmg/Models/BankSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankMmg.Models
+{
+    public class BankSystem
+    {
+        [Key]
+
+        public int ID { get; set; }
+        public String employee_Name { get; set; }
+        public String employee_Position { get; set; }
+        public String employee_Email { get; set; }
+        public String employee_Contact { get; set; }
+
+        public String employee_Address { get; set; }
+    }
+}
