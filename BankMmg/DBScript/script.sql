d9c6e8ab9d6832567f24fce6876a85cc6bc3760a
jot123971 <71738203+jot123971@users.noreply.github.com>
2021-02-22 10:15:25 +1300

    added script file

---
+++ BankMmg/DBScript/script.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[BankSystem] ON
+INSERT INTO [dbo].[BankSystem] ([ID], [employee_Name], [employee_Position], [employee_Email], [employee_Contact], [employee_Address]) VALUES (17, N'jot', N'accountant', N'harjots199828@gmail.com', N'23456', N'3/16 oakley avenue')
+INSERT INTO [dbo].[BankSystem] ([ID], [employee_Name], [employee_Position], [employee_Email], [employee_Contact], [employee_Address]) VALUES (18, N'jassgrewal', N'CA', N'JASS@gmail.com', N'45456', N'83 waterside drive')
+INSERT INTO [dbo].[BankSystem] ([ID], [employee_Name], [employee_Position], [employee_Email], [employee_Contact], [employee_Address]) VALUES (19, N'jot', N'manager', N'harjots199828@gmail.com', N'45456', N'3/16 oakley avenue')
+SET IDENTITY_INSERT [dbo].[BankSystem] OFF
