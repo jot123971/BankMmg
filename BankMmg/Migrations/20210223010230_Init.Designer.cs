aac658837cc8856e1c19df9c0717abb515f2b036
jot123971 <71738203+jot123971@users.noreply.github.com>
2021-02-23 14:33:01 +1300

    Added Migration files

--- BankMmg/Migrations/20210223010230_Init.Designer.cs
+++ BankMmg/Migrations/20210223010230_Init.Designer.cs
@@ -3,14 +3,16 @@ using BankMmg.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankMmg.Migrations
 {
     [DbContext(typeof(BankMmgContext))]
-    partial class BankMmgContextModelSnapshot : ModelSnapshot
+    [Migration("20210223010230_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BankMmg/Migrations/20210223010230_Init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankMmg.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankSystem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    employee_Name = table.Column<string>(nullable: true),
+                    employee_Position = table.Column<string>(nullable: true),
+                    employee_Email = table.Column<string>(nullable: true),
+                    employee_Contact = table.Column<string>(nullable: true),
+                    employee_Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankSystem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankSystem");
+        }
+    }
+}
