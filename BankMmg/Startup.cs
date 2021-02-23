4deec65f556212b7cf08197a4a987bd459128829
jot123971 <71738203+jot123971@users.noreply.github.com>
2021-02-23 14:36:30 +1300

    updated startup file

--- BankMmg/Startup.cs
+++ BankMmg/Startup.cs
@@ -32,6 +32,7 @@ namespace BankMmg
 
             services.AddDbContext<BankMmgContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("BankMmgContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
